cd266339d7a07f21cbfc0a7a1167b0d2a7f5db3e
sebastian.samuelss1 <sebastian.samuelss1@5CD132CZSP.learnet.se>
2023-01-12 15:25:25 +0100

    uppdatering

--- src/programmering_inlämningsuppgift/Programmering_inlämningsuppgift.java
+++ src/programmering_inlämningsuppgift/Programmering_inlämningsuppgift.java
@@ -1,42 +1,117 @@
 package programmering_inlämningsuppgift;
+
 import java.util.Scanner;
-import java.lang.Math;
+
 public class Programmering_inlämningsuppgift {
 
     public static void main(String[] args) {
         Scanner key = new Scanner(System.in);
-        short player=100;
-        short computer1=250;
-        while(player>0 && computer1>0){
-        while (true){
-            
-                
-            
+        short player = 150;
+        short computer1 = 250;
+
+        while (player > 0 && computer1 > 0) {
+            boolean fiendeattack = true;
+            boolean sleep = false;
             System.out.print("Välj attack ");
             byte val = key.nextByte();
-            
-                if (val==1){
-                    System.out.println("Attack 1");
-                    double damage = Math.random()*11;
+            while (sleep == true) {
+                for (int i = 0; i < 4; i++) {
+                    fiendeattack = false;
+
+                }
+            }
+            if (val == 1) {
+
+                double dmg = Math.random() * 31;
+                short damage = (short) dmg;
+                if (damage > 5) {
+                    computer1 = (short) (computer1 - damage);
+
+                } else {
+                    System.out.println("Din attack missade");
+                }
+                while (fiendeattack == true) {
+                    double edmg = Math.random() * 31;
+                    short endamage = (short) edmg;
+                    if (endamage > 20) {
+                        player = (short) (player - endamage);
+                        System.out.println("Fienden träffade");
+                    } else {
+                        System.out.println("Fienden missade");
+                    }
+                    System.out.println("Fiende liv: " + computer1 + " Ditt liv: " + player);
                     break;
                 }
-                else if (val==2){
-                    System.out.println("Attack 2");
+            } else if (val == 2) {
+
+                double dmg = Math.random() * 71;
+                short damage = (short) dmg;
+                if (damage > 40) {
+                    computer1 = (short) (computer1 - damage);
+
+                } else {
+                    System.out.println("Din attack missade");
+                }
+                while (fiendeattack == true) {
+                    double edmg = Math.random() * 31;
+                    short endamage = (short) edmg;
+                    if (endamage > 20) {
+                        player = (short) (player - endamage);
+                        System.out.println("Fienden träffade");
+                    } else {
+                        System.out.println("Fienden missade");
+                    }
+                    System.out.println("Fiende liv: " + computer1 + " Ditt liv: " + player);
                     break;
                 }
-                else if (val==3){
-                    System.out.println("Attack 3");
-                    System.exit(0);
+
+            } else if (val == 3) {
+                double hyp = Math.random() * 101;
+                short hypnosis = (short) hyp;
+                if (hypnosis > 40) {
+                    sleep = true;
+                } else {
+                    System.out.println("Din attack missade");
                 }
-                else if (val==4){
-                    System.out.println("Evade");
+
+                while (fiendeattack == true) {
+                    double edmg = Math.random() * 31;
+                    short endamage = (short) edmg;
+                    if (endamage > 20) {
+                        player = (short) (player - endamage);
+                        System.out.println("Fienden träffade");
+                    } else {
+                        System.out.println("Fienden missade");
+                    }
+                    System.out.println("Fiende liv: " + computer1 + " Ditt liv: " + player);
                     break;
                 }
-                else{
-                    System.out.println("Ogiltigt val");
+            } else if (val == 4) {
+                System.out.println("Evade");
+                double evd = Math.random() * 101;
+                short evade = (short) evd;
+                if (evade > 25) {
+                    System.out.println("Du dodgea attacken");
+                    fiendeattack = false;
+
+                } else {
+                    System.out.println("Du hann inte dodgea");
                 }
-            
+                while (fiendeattack == true) {
+                    double edmg = Math.random() * 31;
+                    short endamage = (short) edmg;
+
+                    player = (short) (player - endamage);
+                    System.out.println("Fienden träffade");
+
+                    System.out.println("Fiende liv: " + computer1 + " Ditt liv: " + player);
+                    break;
+                }
+            } else {
+                System.out.println("Ogiltigt val");
+            }
+
         }
-        } 
     }
+
 }
